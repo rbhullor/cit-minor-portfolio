f576b9fafab2cbc8fb8df96751e3c0bc4b1ce9ed
rbhullor <133918353+rbhullor@users.noreply.github.com>
2023-06-07 16:26:17 -0700

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,59 @@
+# Cit portfolio
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CIT Portfolio</title>
+    <style>
+        body {
+            background-color: #DEB887;
+            margin: 0;
+            padding: 20px;
+            font-family: "Times New Roman", Times, serif;
+        }
+
+        .container {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .column {
+            width: 45%;
+            background-color: #F5DEB3;
+            padding: 20px;
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>CIT Portfolio</h1>
+    <div class="container">
+        <div class="column">
+            <h2>Labs</h2>
+            <ul>
+                <li><a href="https://rbhullor.github.io/cit281-lab1/">Lab 1</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab2/">Lab 2</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab3/">Lab 3</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab4/">Lab 4</a></li>
+                <li><a href="https://rbhullor.github.io/ctit281-lab5/">Lab 5</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab6/">Lab 6</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab7/">Lab 7</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab8/">Lab 8</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-lab9/">Lab 9</a></li>
+            </ul>
+        </div>
+        <div class="column">
+            <h2>Projects</h2>
+            <ul>
+                <li><a href="https://rbhullor.github.io/cit281-project1/">Project 1</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-project2/">Project 2</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-project3/">Project 3</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-project4/">Project 4</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-project5/">Project 5</a></li>
+                <li><a href="https://rbhullor.github.io/cit281-project6/">Project 6</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
